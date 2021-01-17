450fc5fdd9519acb622fad22b4e1240aa508b735
jonny <jonny.huang@chuxingpay.com>
2021-01-17 23:22:10 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { initTracing } from './tracing.middleware';
+
+export { initTracing };
---
+++ src/tracing.middleware.ts
@@ -0,0 +1,33 @@
+import { trace } from '@opentelemetry/api';
+import { LogLevel } from '@opentelemetry/core';
+import { NodeTracerProvider } from '@opentelemetry/node';
+import { BatchSpanProcessor } from '@opentelemetry/tracing';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+// 函数式中间件
+/**
+ * 仅开启了express的tracing
+ * @param serviceName
+ * @param pkg
+ * @param jaegerOptions
+ */
+export function initTracing(serviceName, pkg, jaegerOptions) {
+  const provider: NodeTracerProvider = new NodeTracerProvider({
+    logLevel: LogLevel.INFO,
+    plugins: {
+      express: {
+        enabled: true,
+        path: '@opentelemetry/plugin-express',
+      },
+    },
+  });
+
+  provider.register();
+
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(
+      new JaegerExporter(Object.assign({}, jaegerOptions, { serviceName })),
+    ),
+  );
+  console.log('tracing initialized');
+  return trace.getTracer(pkg.name, pkg.version);
+}
